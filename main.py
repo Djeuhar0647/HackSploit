66fc91d8750e0a072616218e285a3f28bd0f6186
ADN <67873287+Adimir2020@users.noreply.github.com>
2020-07-05 21:07:46 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,43 @@
+import sys
+from PyQt5 import QtCore, QtGui, QtWidgets
+from design import Ui_Dialog
+import random
+
+
+app = QtWidgets.QApplication(sys.argv)
+    
+Dialog = QtWidgets.QDialog()
+ui = Ui_Dialog()
+ui.setupUi(Dialog)
+Dialog.show()
+
+def start_butt():
+    login1 = '0123456789'
+    chars = '+-/*!&$#?=@<>abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890'
+    tel = int('9')
+    zag = ('+79')
+    login2 = ''
+    password2 = ''
+    url_hack = ui.lineEdit.text()
+
+    for i in range(tel):
+        login2 += random.choice(login1)
+
+    for j in range(tel):
+        password2 += random.choice(chars)
+
+    hack = ('Login: ' + zag + login2), ('Password: ' + password2)
+
+    hack1 = str(hack)
+
+    file = open('hack.txt', 'a')
+    file.write('\n' + hack1)
+    file.close
+
+
+    
+    ui.label_5.setText(f'Статус: Данные сохранены!')
+
+ui.pushButton.clicked.connect(start_butt)
+
+sys.exit(app.exec_())
